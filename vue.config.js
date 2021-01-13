e63891359a5b7bbb220d6ca6e4a8de7faef75605
Niklas Pelz <pelznik@gmail.com>
2021-01-13 22:17:10 +0100

    added pwa config
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    https: true
+  },
+  pwa: {
+    name: "TrailRunner",
+    themeColor: "#9c0b0b",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "dev/sw.js"
+    }
+  }
 };
